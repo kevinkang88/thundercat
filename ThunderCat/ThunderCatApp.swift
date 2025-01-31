1a0e776905973df3cca3f4415399a061715283cc
yoon <kevinkang88@gmail.com>
2025-01-31 12:09:02 +0900

    Initial Commit

---
+++ ThunderCat/ThunderCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderCatApp.swift
+//  ThunderCat
+//
+//  Created by Dong Kevin Kang on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ThunderCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThunderCatTests/ThunderCatTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderCatTests.swift
+//  ThunderCatTests
+//
+//  Created by Dong Kevin Kang on 1/31/25.
+//
+
+import Testing
+@testable import ThunderCat
+
+struct ThunderCatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
